ecec536d3e9a059d02d91b6a2e5288a0b14dd9a9
Voltra <voltraneo@gmx.fr>
2017-07-21 23:24:01 +0200

    Compatibility Update : AMD, CommonJS, NPM, etc...

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# fetchJSON
-A Javascript library that allows to abstract all boilerplate from retrieving JSON data using the native ES6 fetch API
+# fetchJSON #
+A Javascript library that allows to abstract all boilerplate from retrieving JSON data using the native ES6 fetch API.
+
+
+----------
+
+
+
+Simple and easy to use, it only abstracts away all the boilerplate required for fetching json data.
+No other javascript library is required to use this library.
+
+
+----------
+## Installation/Use ##
+Just like every library should, I tried to make fetchJSON compatible with quite everything I am aware of.
+
+fetchJSON currently supports regular use (via HTML script tag), npm and require (and commonJS), AMD (and probably ES6 modules ?).
+
+Therefore, here are two ways to install this library:
+
+```html
+<html>
+	<!-- [...] -->
+	<head>
+		<!-- [...] -->
+		<script src="path/to/fetchJSON.js"></script>
+		<!-- [...] -->
+	</head>
+	<!-- [...] -->
+</html>
+```
+
+----------
+
+```
+npm install fetch_json
+```
+```javascript
+const fetchJSON = require("fetch_json");
+```
+
+## Simple use case ##
+Sometimes, you just have that json file which's sole purpose is configuration.
+With fetchJSON, getting the data from this file is very easy:
+
+```javascript
+//With fetchJSON
+let config_data;
+fetchJSON("../../someFolder/someFile.json", data=>config_data=data);
+
+//Without fetchJSON
+let config_data;
+fetch("../../someFolder/someFile.json").then(response=>{
+	/*gather headers*/
+	if(/*there's json in there*/)
+		return response.json().then(data=>{
+			/*some manipulation*/
+			let config_data = data;
+			//finally !
+		});
+	else
+		/*handle error*/
+})
+
+```
+
+Once loaded, the data can be used like this: 
+```javascript
+let config_data;
+fetchJSON("../../someFolder/someFile.json", data=>config_data=data)
+.then(()=>{
+	/* use config_data to configure your application*/
+});
+```
+
+## Questions/Suggestions ##
+Please fill free to ask for help / post suggestions on my github repository, I'll be more than glad to take care of your problems/concerns.
--- fetchJSON.js
+++ fetchJSON.js
@@ -5,30 +5,42 @@
 *
 *@return the Promise object of the fetch request
 */
-function fetchJSON(path, functor){
-    if(  (typeof functor == typeof (x=>x)) && (typeof path == typeof "42xyz")  ){
-        const f = fetch(path);
-        
-        f.then((response)=>{
-            var contentType= response.headers.get("content-type");
-            
-            if(contentType && contentType.includes("application/json"))
-                return response.json().then( jsonData=>functor(jsonData) );
-            else{
-                //console.error("fetchJSON.js : Something went wrong during data inspection (data is not JSON or file is unreachable)");
-                throw new Error("Something went wrong during data inspection (data is not JSON or couldn't reach file)");
-                return null;
-            }
-        });
-        
-        return f;
+
+(function UniversalModuleDefinition(root, factory){
+    if(typeof exports === 'object' && typeof module === 'object')
+        module.exports = factory();
+    else if(typeof define === 'function' && define.amd)
+        define("fetchJSON", [], factory);
+    else if(typeof exports === 'object')
+        exports["fetchJSON"] = factory();
+    else
+        root["fetchJSON"] = factory();
+})(this, function(){
+    return function(path, functor){
+        if(  (typeof functor == typeof (x=>x)) && (typeof path == typeof "42xyz")  ){
+            const f = fetch(path);
+
+            f.then((response)=>{
+                var contentType= response.headers.get("content-type");
+
+                if(contentType && contentType.includes("application/json"))
+                    return response.json().then( jsonData=>functor(jsonData) );
+                else{
+                    //console.error("fetchJSON.js : Something went wrong during data inspection (data is not JSON or file is unreachable)");
+                    throw new Error("Something went wrong during data inspection (data is not JSON or couldn't reach file)");
+                    return null;
+                }
+            });
+
+            return f;
+        }
+        else{
+            //console.error("fetchJSON.js : The first argument must be a string, the second argument must be a function");
+            if(typeof path != typeof "42xyz")
+                throw new TypeError("The 1st argument must be a string");
+            if(typeof functor != typeof (x=>x))
+                throw new TypeError("The 2nd argument must be a function");
+            return null;
+        }
     }
-    else{
-        //console.error("fetchJSON.js : The first argument must be a string, the second argument must be a function");
-        if(typeof path != typeof "42xyz")
-            throw new TypeError("The 1st argument must be a string");
-        if(typeof functor != typeof (x=>x))
-            throw new TypeError("The 2nd argument must be a function");
-        return null;
-    }
-}
+});
